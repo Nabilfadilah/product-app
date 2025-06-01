96b5cd44bbc60dde46f2b80e3a2041a83e462012
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-01 20:01:19 +0700

    Membuat template footer

---
+++ public/templates/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="bg-gray-100 mt-20">
+    <div class="max-w-6xl mx-auto px-4 py-8 text-center text-gray-600">
+        &copy; <?= date("Y") ?> Percetakan Aliza. All rights reserved.
+    </div>
+</footer>
+
+</body>
+
+</html>
