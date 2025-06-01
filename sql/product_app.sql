cf8872d26a1736bcbe37d5559d393cc8bf5d40b2
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-01 19:54:49 +0700

    Menambah table user sql

--- sql/product_app.sql
+++ sql/product_app.sql
@@ -10,8 +10,22 @@ CREATE TABLE products (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') NOT NULL DEFAULT 'user'
+);
+
+
 -- Optional contoh data
 INSERT INTO products (name, category, price, stock)
 VALUES
 ('Pulpen Snowman', 'Alat Tulis', 3500, 100),
 ('Kertas A4', 'Kertas', 50000, 25);
+
+
+INSERT INTO users (username, password, role)
+VALUES ('admin', MD5('admin123'), 'admin');
+
+ALTER TABLE users CHANGE username email VARCHAR(100) NOT NULL UNIQUE;
